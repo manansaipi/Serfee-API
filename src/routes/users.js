d253209415a2a52f5d44f034926989e462a638a2
manansaipi <abdul.saipi@student.president.ac.id>
2023-05-13 07:07:02 +0700

    run linter on push

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,6 +4,6 @@ const usersController = require('../controller/users')
 const router = express.Router()
 
 // CREATE user - POST
-router.post('/', usersController.createUser) 
+router.post('/', usersController.createUser) // path "/" will refer to /users because grouping in index.js file so no need to write'/users'
 
 module.exports = router
