88edf2372dd37c9e54c1bb6d0b8a7da4e15914fc
manansaipi <abdul.saipi@student.president.ac.id>
2023-05-14 11:54:30 +0700

    fix lint error

--- src/controller/users.js
+++ src/controller/users.js
@@ -14,7 +14,7 @@ const createUser = async (req, res) => {
     }
 
     try {
-        await UsersModel.createNewUser(body);
+        await UsersModel.createNewUser(body); // excecute query
         return res.status(201).json({
             message: "CREATE new user success", 
             data: body
@@ -29,88 +29,86 @@ const createUser = async (req, res) => {
 
 const getAllUsers = async (req, res) => {
     try {
-        const [data] = await UsersModel.getAllUsers()
+        const [data] = await UsersModel.getAllUsers(); // excecute query
         res.json({
-            message: 'GET all users',
+            message: "GET all users",
             data
-        })
+        });
     } catch (error) {
         res.status(500).json({
-            message: 'Server Error',
+            message: "Server Error",
             serverMessage: error,
-        })
+        });
     }
-}
+};
 // READ a user by ID
 const getUserById = async (req, res) => {
     // Get the user ID from the request parameters
     const id = req.params.id;
-    console.log('id: ', id) 
-    // console.log(req.params) params -> parameter
+    console.log("id: ", id);
     try {
-        const [data] = await UsersModel.getUser(id)
+        const [data] = await UsersModel.getUser(id); // excecute query
         res.json({
-            message: 'Get user success',
+            message: "Get user success",
             data
-        }) 
+        });
     } catch (error) {
         res.status(500).json({
-            message: 'Server Error',
+            message: "Server Error",
             serverMessage: error,
-        })
+        });
     }
 };
 
 const updateUserById = async (req, res) => {
-    const { id } = req.params.id
-    const { body } = req
-    console.log('id: ', id) 
-    // console.log(req.params) params -> parameter
+    const { id } = req.params.id;
+    const { body } = req;
+    console.log("id: ", id);
     try {
-        await UsersModel.updateUser(body, id)
+        await UsersModel.updateUser(body, id); // excecute query
         res.json({
-            message: 'UPDATE user success',
+            message: "UPDATE user success",
             data: {
                 id,
                 ...body
             }
-        }) 
+        });
     } catch (error) {
         res.status(500).json({
-            message: 'Server Error',
+            message: "Server Error",
             serverMessage: error,
-        })
+        });
     }
-}
+};
 
 const deleteUserById = async (req, res) => {
-    const { id } = req.params
-    const { body } = req
-    console.log('id: ', id)
+    const { id } = req.params;
+    const { body } = req;
+    console.log("id: ", id);
     try {
-        const [data] = await UsersModel.getUser(id)
-        console.log(data)
-        if (data === '') {
+        const [data] = await UsersModel.getUser(id); // excecute query
+        console.log(data);
+        if (data === "") {
             res.status(404).json({
-                message: 'id not found'
-            })
+                message: "id not found"
+            });
         } else {
-            await UsersModel.deleteUser(id)
+            await UsersModel.deleteUser(id); // excecute query
             res.json({
-                message: 'DELETE user success',
+                message: "DELETE user success",
                 data: {
                     id,
                     body
                 }
-            })
+            });
         }
     } catch (error) {
         res.status(500).json({
-            message: 'Server Error',
+            message: "Server Error",
             serverMessage: error,
-        })    
+        });
     }
-}
+};
 
 module.exports = {
     createUser,
